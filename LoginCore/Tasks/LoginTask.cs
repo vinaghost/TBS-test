d1b47e1aed15235e0d9d15a5fad7abed61037a02
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-09-03 17:01:07 +0700

    login task

---
+++ LoginCore/Tasks/LoginTask.cs
@@ -0,0 +1,18 @@
+﻿using FluentResults;
+using LoginCore.Commands;
+using MainCore.Tasks;
+using Splat;
+
+namespace LoginCore.Tasks
+{
+    public class LoginTask : AccountTask
+    {
+        public override async Task<Result> Execute()
+        {
+            if (CancellationToken.IsCancellationRequested) return Result.Ok();
+            var loginCommand = Locator.Current.GetService<ILoginCommand>();
+            await loginCommand.Execute(AccountId);
+            return Result.Ok();
+        }
+    }
+}
---
+++ MainCore/Tasks/TaskBase.cs
@@ -0,0 +1,14 @@
+﻿using FluentResults;
+using MainCore.Enums;
+
+namespace MainCore.Tasks
+{
+    public abstract class TaskBase
+    {
+        public StageEnums Stage { get; set; }
+        public DateTime ExecuteAt { get; set; }
+        public CancellationToken CancellationToken { get; set; }
+
+        public abstract Task<Result> Execute();
+    }
+}
